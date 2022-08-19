3d53be1bac8673c633b6e41e2490f22e8ffb7fdb
Leila Abdelrahman <leila.zakaria@gmail.com>
2022-08-19 10:27:47 -0400

    [ADD] Added main source code

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Leila on 8/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Turtle Rock").font(.title).foregroundColor(.black)
+                .padding()
+            
+            HStack {
+                Text("Joshua Tree National Park").font(.subheadline)
+                i
+            }
+         
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Leila on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
